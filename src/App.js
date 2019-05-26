a6eca0aa6d95820c09318a0bfa045ed9c95096cf
Daniel Ruales <danielmruales@gmail.com>
2019-05-26 15:31:36 -0600

    created home folder with js and css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './Home/Home.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello I will begin working on this tomorrow</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Okay time to start coding again</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
