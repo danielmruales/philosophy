5bbd154c16eafa958c217dc0a9d405f4d7532db5
Daniel Ruales <danielmruales@gmail.com>
2019-05-28 20:10:47 -0600

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Home from './Home/Home.js'
 import Nav from './Nav/Nav.js'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav/>
-      <Home/>
+      <Switch>
+        <Route exact path= '/' component={Home}/>
+      </Switch>
     </div>
   );
 }
